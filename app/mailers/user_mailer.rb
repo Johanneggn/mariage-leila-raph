d6c3afb992bd36b6359b4ca11f9cc5f215bd92e9
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2022-03-07 13:24:58 +0100

    modif site front

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,6 @@ class UserMailer < ApplicationMailer
   #
   def invitation(guest)
     @guest = guest
-    mail(to: 'johanne.gueguen@gmail.com', subject: 'Réponse invitation')
+    mail(to: 'leila.monvoisin2017@gmail.com', subject: 'Réponse invitation')
   end
 end
